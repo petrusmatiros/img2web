f2014ecb50a3a307b50cd4e491a2877cb5015618
Petrus Matiros <petrus.matiros@gmail.com>
2023-11-07 00:18:44 +0100

    feat: init project and add dev code

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# img2web
+<p align="center">
+<h1 align="center">img2web</h1>
+</p>
+
+## Table of Contents
+- [Table of Contents](#table-of-contents)
+- [Website URL](#website-url)
+- [Project description](#project-description)
+- [Installation](#installation)
+  - [Dependencies](#dependencies)
+
+## Website URL
+-
+
+## Project description
 A CLI tool for converting bulk images to compressed, resized WEBP images
+
+
+
+## Installation
+```
+npm i
+node index.js
+```
+
+To get a development build, run `npm run dev`
+
+### Dependencies
+```json
+
+```
---
+++ index.js
@@ -0,0 +1,136 @@
+const sharp = require("sharp");
+const fs = require("fs").promises;
+
+const folderPathInput = "./input";
+const folderPathOutput = "./output";
+
+async function getFileNames() {
+  try {
+    const files = await fs.readdir(folderPathInput);
+    return files.filter(async (file) => {
+      const stats = await fs.lstat(`${folderPathInput}/${file}`);
+      return stats.isFile();
+    });
+  } catch (err) {
+    console.error("Error reading directory:", err);
+    return [];
+  }
+}
+
+async function getFilesizeInBytes(filename) {
+  try {
+    var stats = await fs.stat(filename);
+  } catch (err) {
+    console.error("Error reading file:", err);
+    return;
+  }
+  var fileSizeInBytes = stats.size;
+  return fileSizeInBytes;
+}
+
+const BYTE_SIZE = {
+  B: 1,
+  KB: 1000,
+  MB: 1000 * 1000,
+  GB: 1000 * 1000 * 1000,
+};
+
+function getFileSize(fileSize, byteSize) {
+  function getKeyByValue(object, value) {
+    return Object.keys(object).find((key) => object[key] === value);
+  }
+  const key = getKeyByValue(BYTE_SIZE, byteSize);
+  if (!fileSize) return "unknown";
+  if (!key) return "unknown";
+  return `${(fileSize / byteSize).toFixed(2)} ${key}`;
+}
+
+const args = {
+  fileInfo: false,
+  byteSize: BYTE_SIZE.KB,
+  quality: 75,
+};
+
+run(args);
+
+async function run({ fileInfo = false, byteSize = BYTE_SIZE.MB, quality = 75 }) {
+  try {
+    const fileNames = await getFileNames();
+
+    for (const fileName of fileNames) {
+      let fileSize = undefined;
+      if (!fileInfo) {
+        try {
+          fileSize = await getFilesizeInBytes(`${folderPathInput}/${fileName}`);
+        } catch (err) {
+          console.error("Error reading file:", err);
+        }
+      }
+
+      try {
+        console.log("--------------------------------------------------");
+        console.log(`Converting file: ${fileName}`);
+        console.log(`[INPUT] file size: ${getFileSize(fileSize, byteSize)}`);
+
+        const inputImagePath = `${folderPathInput}/${fileName}`;
+        const fileFormatOutput = ".webp";
+        const fileNameOutput = fileName.split(".")[0];
+
+        const outputImagePath = `${folderPathOutput}/${fileNameOutput}-compressed${fileFormatOutput}`;
+
+        // Define the compression and resizing options
+        const options = {
+          quality: quality,
+          width: 1920,
+          height: 1080,
+          fit: "contain",
+          withoutEnlargement: true,
+          fastShrinkOnLoad: true,
+        };
+
+        const image = sharp(inputImagePath);
+        try {
+          const metadata = await image.metadata();
+          if (metadata.height > metadata.width) {
+            const aspectRatio = metadata.height / metadata.width;
+            options.height = Math.floor(options.width * aspectRatio);
+          } else if (metadata.width > metadata.height) {
+            const aspectRatio = metadata.width / metadata.height;
+            options.width = Math.floor(options.height * aspectRatio);
+          } else if (metadata.width === metadata.height) {
+            options.height = options.width;
+          }
+
+          const processed = image.resize(options).webp(options);
+
+          try {
+            const info = await processed.toFile(outputImagePath);
+
+            if (fileSize) {
+              console.log(
+                `[OUTPUT] file size: ${getFileSize(
+                  info.size,
+                  byteSize
+                )} [-${getFileSize(fileSize - info.size, byteSize)}] [-${(
+                  ((fileSize - info.size) / fileSize) *
+                  100
+                ).toFixed(2)}%]`
+              );
+            }
+            console.log(
+              `[OUTPUT] file info: [${info.format}] ${info.width}x${info.height}`
+            );
+          } catch (err) {
+            console.error("Error writing file:", err);
+          }
+        } catch (err) {
+          console.error("Error reading file:", err);
+        }
+      } catch (err) {
+        console.error("Error reading file:", err);
+      }
+    }
+  } catch (err) {
+    console.error("Error reading directory:", err);
+  }
+}
